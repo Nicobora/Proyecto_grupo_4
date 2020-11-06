acdf59cb8d3c30d3fe9aaaec4164ed72373dc5fa
Mar <marianacanete1@gmail.com>
2020-11-06 20:19:20 -0300

    primer funcion de js

--- layouts/js/Index.js
+++ layouts/js/Index.js
@@ -3,3 +3,18 @@
 	});	
 
 	
+	function recuperacion() {
+
+		var num = document.getElementById("emailRecuperacion").value.indexOf("@");
+		var email = document.getElementById("emailRecuperacion").value;
+
+		if (num == -1 ) {
+
+			alert("No es un e-mail valido!");
+		}
+
+		else {
+
+			alert("¡Listo! Te enviamos un email a " + email);
+		}
+	}
